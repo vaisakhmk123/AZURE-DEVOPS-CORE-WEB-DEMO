0ce156be64e8269d8243c84a9afdd95132b53ae9
VAISAKH MK <mk.vaisakh@outlook.com>
2021-04-30 23:20:25 +0530

    Initial checking

---
+++ AZURE-DEVOPS-CORE-WEB-DEMO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AZURE_DEVOPS_CORE_WEB_DEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
